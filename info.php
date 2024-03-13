9acc1b0e9ef0fdfeaa2b2a43ffeac55656ddac02
yuhei1518 <yuhei23491518@gmail.com>
2024-03-13 11:15:12 +0900

    Add index

---
+++ info.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/particles.js"></script>
+  <title>好きなこと</title>
+
+</head>
+
+<body>
+  <div class="cursor">
+
+    <div id="particles-js"></div>
+    <div id="wrapper">
+    </div>
+
+    <!---------------------------------------------------------------------------------->
+    <header>
+      <div class="header">
+        <div class="header_logo">
+          <a href="info.php"><img src="images/logo.png" alt=""></a>
+        </div>
+        <div class="header_menu">
+          <input type="checkbox" id="menu-btn-check">
+          <label for="menu-btn-check" class="menu-btn"><span></span></label>
+
+          <div class="header_menu-content">
+            <ul>
+              <li>
+                <a href="#">1</a>
+              </li>
+              <li>
+                <a href="#">2</a>
+              </li>
+              <li>
+                <a href="#">3</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </header>
+
+    <!---------------------------------------------------------------------------------->
+
+    <main>
+      <div class="main">
+        <div class="main_logo">
+          <b>好<span>きな</span>事<span>ペー</span>ジ</b>
+        </div>
+
+        <a href="form.php" id="btn" target="_blank">問い合わせフォーム</a>
+      </div>
+    </main>
+
+
+    <!---------------------------------------------------------------------------------->
+
+    <nav>
+      <div class="nav">
+        <div class="nav_contents">
+          <div class="nav_contents-news">
+            <div class="nav_contents-news-1">
+              <span class="split-n">
+                <span>A</span>
+              </span>
+              <span class="split-n">
+                <span>N</span>
+              </span>
+              <span class="split-n">
+                <span>I</span>
+              </span>
+              <span class="split-n">
+                <span>M</span>
+              </span>
+              <span class="split-n">
+                <span>E</span>
+              </span>
+            </div>
+            <div class="nav_contents-news-2">
+              <div class="nav_contents-anime-1">
+                <div class="nav_contents-anime-1-day">
+                  2019.04.06 ［鬼滅の刃］
+                </div>
+                <div class="nav_contents-anime-1-content">
+                  舞台は大正時代。親兄弟を人喰い鬼に殺されてしまった少年・竈門 炭治郎（かまど たんじろう）
+                  が、唯一生き残るも鬼となってしまった妹の禰豆子（ねずこ）を人間に戻す方法を探す物語。
+                </div>
+              </div>
+              <div class="nav_contents-anime-1">
+                <div class="nav_contents-anime-2-day">
+                  2023.04.01 ［僕の心のヤバいやつ］
+                </div>
+                <div class="nav_contents-anime-2-content">
+                  憂鬱とした日々を送る中学2年生・市川が、今最も殺したい相手No.1は、学校一の美少女・山田。
+                  スクールカースト上位にいる山田とは言葉を交わす機会もない市川だが・・・
+                </div>
+              </div>
+              <div class="nav_contents-anime-1">
+                <div class="nav_contents-anime-3-day">
+                  2022.01.08 ［その着せ替え人形は恋をする］
+                </div>
+                <div class="nav_contents-anime-3-content">
+                  雛人形を作る職人を夢見る内気な男子高校生とギャルでオタクで天真爛漫な女子高生が、コスプレを
+                  通して一途な恋心を育んでいくラブコメ。
+                </div>
+              </div>
+              <div class="nav_contents-anime-1">
+                <div class="nav_contents-anime-4-day">
+                  2007.02.15 ［NARUTO］
+                </div>
+                <div class="nav_contents-anime-4-content">
+                  里一番の落ちこぼれである主人公「うずまきナルト」が己の忍道を貫きながら
+                  敵と戦い、仲間と共に成長していく姿を描く王道ストーリー。
+                </div>
+              </div>
+              <div class="nav_contents-anime-1">
+                <div class="nav_contents-anime-4-day">
+                  2016.04.03 ［機動戦士　ガンダムUC　ユニコーン］
+                </div>
+                <div class="nav_contents-anime-4-content">
+                  工業スペースコロニーに住むバナージは、ある日、オードリーと名乗る少女と出会う。戦場と化した
+                  コロニーでオードリーを探すバナージはユニコーンガンダムとの運命的な出会いを果たす。
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="nav_contents-media">
+            <div class="nav_contents-media-1">
+              <span class="split-n">
+                <span>D</span>
+              </span>
+              <span class="split-n">
+                <span>R</span>
+              </span>
+              <span class="split-n">
+                <span>A</span>
+              </span>
+              <span class="split-n">
+                <span>M</span>
+              </span>
+              <span class="split-n">
+                <span>A</span>
+              </span>
+            </div>
+            <div class="nav_contents-news-2">
+              <div class="nav_contents-drama-1">
+                <div class="nav_contents-drama-1-day">
+                  "2018.01.04 ［運命のように君を愛している］"
+                </div>
+                <div class="nav_contents-drama-1-content">
+                  CEOであるイ・ゴンは、 短命の家系のため周囲から常に結婚と跡継ぎを急かされていた。
+                  一方、平凡な会社員のミヨンは、 優しすぎるあまりにいつも損ばかりしている。
+                  そんな二人がゴンを陥れようとする策略により一夜を共にしてしまう。のちに妊娠が発覚し
+                  戸惑いながらも結婚を決意する2人だったが...。
+                </div>
+              </div>
+              <div class="nav_contents-drama-1">
+                <div class="nav_contents-drama-2-day">
+                  "2017.05.22 ［サムマイウェイ］"
+                </div>
+                <div class="nav_contents-drama-1-content">
+                  現在はダニ駆除業の仕事をするコ・ドンマン。幼なじみのチェ・エラもアナウンサーになる夢を諦めて
+                  今はデパートの案内係。2人は同じアパートに向かい合って住み、何でも言える仲。
+                  ドンマンの前には有名アナウンサーの元カノが、エラの前にはハイスペックな新カレが登場。
+                  ただの親友のはずだった2人の仲に微妙な変化が生じ始める...。
+                </div>
+              </div>
+              <div class="nav_contents-drama-1">
+                <div class="nav_contents-drama-3-day">
+                  "2018.07.27 ［私のIDはカンナム美人］"
+                </div>
+                <div class="nav_contents-drama-1-content">
+                  幼い頃から容姿のことでいじめを受けていたミレは、普通の人のように生きたいと美容整形を決意。
+                  生まれ変わった姿で大学に入学するが、中学の同級生だったギョンソクと再会してしまう。
+                  周囲に昔の顔が知られるのを恐れたミレは、ギョンソクに秘密にしてほしいと頼むが...。
+
+                </div>
+              </div>
+              <div class="nav_contents-drama-1">
+                <div class="nav_contents-drama-4-day">
+                  "2022.04.22 ［流れ星］"
+                </div>
+                <div class="nav_contents-drama-1-content">
+                  トップスターのテソンとスターを陰で支える事務所社員のハンビョル、違う立場の二人は
+                  大学時代に知り合い犬猿の仲の二人がいつの間にか...。
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!---------------------------------------------------------------------------------->
+
+    <div class="menu1">
+
+    </div>
+    <div class="menu2">
+
+    </div>
+    <div class="menu3">
+
+    </div>
+
+    <!---------------------------------------------------------------------------------->
+
+    <footer>
+      <div class="footer">
+        <h3>こんなお問い合わせがあります</h3>
+        <div class="footer-contents">
+          <table border="1">
+            <tr>
+              <th>ユーザー名</th>
+              <th>問い合わせ内容</th>
+              <th>好きなアニメ</th>
+            </tr>
+            <?php
+            try {
+              $pdo = new PDO("mysql:host=localhost; dbname=like; charset=utf8", "root", "");
+              if (!$pdo) {
+                exit('データベースに接続できませんでした。');
+              }
+
+              $result = $pdo->query('SELECT * FROM user WHERE hyouji = 1 ORDER BY time asc LIMIT 5');
+              if (!$result) {
+                exit('クエリの実行に失敗しました。');
+              }
+
+              while ($data = $result->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                echo "<td>" . $data['name'] . "</td>";
+                echo "<td>" . $data['coment'] . "</td>";
+                echo "<td>" . $data['anime'] . "</td>";
+                echo "</tr>";
+              }
+            } catch (PDOException $e) {
+              exit('クエリの実行中にエラーが発生しました。エラー: ' . $e->getMessage());
+            } finally {
+              $pdo = null;
+            }
+            ?>
+          </table>
+        </div>
+      </div>
+    </footer>
+
+
+    <!---------------------------------------------------------------------------------->
+
+    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <!--自作のJS-->
+    <script src="js/back.js"></script>
+
+
+  </div>
+
+</body>
+
+</html>
